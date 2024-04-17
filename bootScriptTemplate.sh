cdb298e65c6b7b525c7a068b7de1ae1f0b8ec41e
CoderCIRNO <treant@qq.com>
2024-04-17 19:56:02 +0800

    Simplify boot Script
    a.t.
    
    Signed-off-by: CoderCIRNO <treant@qq.com>

--- bootScriptTemplate.sh
+++ bootScriptTemplate.sh
@@ -1,32 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # survivor
 cd ~/
 git clone https://github.com/CoderCIRNO/SurvivorVPN.git
 cd ~/SurvivorVPN
-touch pingServer.sh
-chmod +x pingServer.sh
-# 此处 3 项配置
-cat > pingServer.sh<<-EOF
-PingServerPort=
-PingServerToken=""
-PingAim=""
-systemctl stop firewalld
-export PingServerPort
-export PingServerToken
-export PingAim
-cd ~/SurvivorVPN
-python3 pingServer.py >> ~/pingServer.log
-systemctl start firewalld
-EOF
 
-./pingServer.sh
-
-cd ~/SurvivorVPN
-touch regularRun.sh
-chmod +x regularRun.sh
-# 此处 11 项配置（与上面的 3 项存在重复）
-cat > regularRun.sh<<-EOF
+cat > envParams<<-EOF
 # Cloudflareconfig
 ZoneID=""
 AimDNSRecoreID=""
@@ -35,8 +14,8 @@ CloudFlareToken=""
 
 # Vultrconfig
 VultrToken=""
-DefaultRegion=""
-DefaultPlan=""
+DefaultRegion="sgp"
+DefaultPlan="vc2-1c-1gb"
 StartUpScriptID=""
 
 # PingServer
@@ -55,6 +34,25 @@ export StartUpScriptID
 export PingServerPort
 export PingServerToken
 export PingAim
+
+EOF
+
+cp envParams pingServer.sh
+chmod +x pingServer.sh
+cat >> pingServer.sh<<-EOF
+systemctl stop firewalld
+cd ~/SurvivorVPN
+echo "#### Start Ping Server ####"
+python3 pingServer.py
+systemctl start firewalld
+EOF
+
+./pingServer.sh >> ~/pingServer.log
+
+
+cp envParams regularRun.sh
+chmod +x regularRun.sh
+cat >> regularRun.sh<<-EOF
 cd ~/SurvivorVPN
 python3 pingCheckUpdate.py regularcheck >> ~/autorun.log
 EOF
@@ -63,12 +61,11 @@ EOF
 cd ~/SurvivorVPN
 chmod +x shadowsocksR.sh
 
-# 此处 5 项配置
 shadowsocksport=
 shadowsockspwd=""
-shadowsockscipher=""
-shadowsockprotocol=""
-shadowsockobfs=""
+shadowsockscipher="aes-256-cfb"
+shadowsockprotocol="origin"
+shadowsockobfs="plain"
 
 export shadowsocksport
 export shadowsockspwd
@@ -80,7 +77,6 @@ export shadowsockobfs
 
 # add regular check crontab
 cd ~/SurvivorVPN
-touch regular.crontab
 cat > regular.crontab<<-EOF
 0 0,3,6,9,12,15,18,21 * * * cd ~/SurvivorVPN; ./regularRun.sh
 EOF
